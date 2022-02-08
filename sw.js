864e93f6a34e24508a60235170265428c3b504a0
Kivi <>
2022-02-08 22:10:05 +0000

    advanced caching - dynamic caching and fallback page.

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
-const staticCacheName = 'site-static'
+const staticCacheName = 'site-static-v2'
+const dynamicCacheName = 'site-dynamic-v1'
 const assets = [
     './',
     './index.html',
     './js/script.js',
-    'https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css'
+    'https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css',
+    './fallback.html'
 ]
 
 // listen for installed service worker
@@ -18,13 +20,26 @@ self.addEventListener('install', event => {
 
 // listen for activated service worker
 self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+                .map(key => caches.delete(key))
+            )
+        })
+    )
 })
 
 // listen to fetch event
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then(cacheRes => {
-            return cacheRes || fetch(event.request);
-        })
+            return cacheRes || fetch(event.request).then(fetchRes => {
+                return caches.open(dynamicCacheName).then(cache => {
+                    cache.put(event.request.url, fetchRes.clone())
+                    return fetchRes
+                })
+            });
+        }).catch(() => caches.match('./fallback.html'))
     )
 });
