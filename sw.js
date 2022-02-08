78587499f51160c71293977e0d304ff865f6e582
Kivi <>
2022-02-08 21:00:16 +0000

    precache and fetch cache to enable offline.

--- sw.js
+++ sw.js
@@ -1,14 +1,30 @@
+const staticCacheName = 'site-static'
+const assets = [
+    './',
+    './index.html',
+    './js/script.js',
+    'https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css'
+]
+
 // listen for installed service worker
 self.addEventListener('install', event => {
-    console.log('service worker has been installed')
+    event.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log('caching shell assests')
+            cache.addAll(assets)
+        })
+    )
 })
 
 // listen for activated service worker
 self.addEventListener('activate', event => {
-    console.log('service worker has been activated')
 })
 
 // listen to fetch event
 self.addEventListener('fetch', (event) => {
-    console.log('fetch event', event)
+    event.respondWith(
+        caches.match(event.request).then(cacheRes => {
+            return cacheRes || fetch(event.request);
+        })
+    )
 });
