8e80e52b33f2691d320b804f9c9176db98856287
Kivi <>
2022-02-08 19:54:38 +0000

    testing.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // only run if browser supports service workers
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("../sw.js") // register the sw
+  navigator.serviceWorker.register("./sw.js") // register the sw
     .then((reg) => console.log("service worker is registered", reg))
     .catch((err) => console.log("service worker NOT registered", err));
 }
