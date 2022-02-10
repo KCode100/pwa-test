49095f7ad269d8d78a05fa2f9a93f12c912046fa
Kivi <>
2022-02-10 22:54:33 +0000

    fix with CDN

--- js/script.js
+++ js/script.js
@@ -1,6 +1,43 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js';
+import { getFirestore, collection, getDocs, enableIndexedDbPersistence } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAbZZdzAEqksS97eyREomtyR_EIfiLC7BA",
+  authDomain: "pwa-test-5561e.firebaseapp.com",
+  projectId: "pwa-test-5561e",
+  storageBucket: "pwa-test-5561e.appspot.com",
+  messagingSenderId: "818373461234",
+  appId: "1:818373461234:web:b16b87328afa31b089bc6b",
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+enableIndexedDbPersistence(db)
+  .catch((err) => {
+      if (err.code == 'failed-precondition') {
+          // Multiple tabs open, persistence can only be enabled
+          // in one tab at a a time.
+          // ...
+      } else if (err.code == 'unimplemented') {
+          // The current browser does not support all of the
+          // features required to enable persistence
+          // ...
+      }
+  });
+
+// Get a list of cities from your database
+async function getUsers(db) {
+  const usersCol = collection(db, 'users');
+  const usersSnapshot = await getDocs(usersCol);
+  const usersList = usersSnapshot.docs.map(doc => doc.data());
+  console.log(usersList);
+}
+
+getUsers(db)
+
 // only run if browser supports service workers
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("./sw.js") // register the sw
+  navigator.serviceWorker.register("/sw.js") // register the sw
     .then((reg) => console.log("service worker is registered", reg))
     .catch((err) => console.log("service worker NOT registered", err));
 }
--- sw.js
+++ sw.js
@@ -1,61 +1,60 @@
-const staticCacheName = 'site-static-v2'
-const dynamicCacheName = 'site-dynamic-v1'
+const staticCacheName = "site-static-v2";
+const dynamicCacheName = "site-dynamic-v1";
 const assets = [
-    './',
-    './index.html',
-    './js/script.js',
-    'https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css',
-    './fallback.html'
-]
-
-// cache size limit function
-// const limitCacheSize = (name, size) => {
-//     caches.open(name).then(cache => {
-//         cache.keys().then(keys => {
-//             if(keys.length > size) {
-//                 cache.delete(keys[0]).then(limitCacheSize(name, size))
-//             }
-//         })
-//     })
-// }
+  "/",
+  "/index.html",
+  "/js/script.js",
+  "https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css",
+  "/fallback.html",
+];
 
 // listen for installed service worker
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(staticCacheName).then(cache => {
-            console.log('caching shell assests')
-            cache.addAll(assets)
-        })
-    )
-})
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log("caching shell assests");
+      cache.addAll(assets);
+    })
+  );
+});
 
 // listen for activated service worker
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(keys
-                .filter(key => key !== staticCacheName && key !== dynamicCacheName)
-                .map(key => caches.delete(key))
-            )
-        })
-    )
-})
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== staticCacheName && key !== dynamicCacheName)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+});
 
 // listen to fetch event
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
+  if (event.request.url.indexOf("firestore.googleapis.com") === -1) {
     event.respondWith(
-        caches.match(event.request).then(cacheRes => {
-            return cacheRes || fetch(event.request).then(fetchRes => {
-                return caches.open(dynamicCacheName).then(cache => {
-                    cache.put(event.request.url, fetchRes.clone())
-                    // limitCacheSize(dynamicCacheName, 20)
-                    return fetchRes
-                })
-            });
-        }).catch(() => {
-            if (event.request.url.indexOf('.html') > -1) {
-                return caches.match('./fallback.html')
-            }
+      caches
+        .match(event.request)
+        .then((cacheRes) => {
+          return (
+            cacheRes ||
+            fetch(event.request).then((fetchRes) => {
+              return caches.open(dynamicCacheName).then((cache) => {
+                cache.put(event.request.url, fetchRes.clone());
+                return fetchRes;
+              });
+            })
+          );
         })
-    )
+        .catch(() => {
+          if (event.request.url.indexOf(".html") > -1) {
+            return caches.match("/fallback.html");
+          }
+        })
+    );
+  }
 });
+
+console.log('talkingfrom service worker')
